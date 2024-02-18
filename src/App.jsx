fc1424c59dcda2c7cccaa1fcdf8d272c9c62656d
Arthur Florenço <135295547+arthurflorenco@users.noreply.github.com>
2024-02-18 02:42:34 +0000

    project update

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import About from './components/About'
+import Home from './components/Home'
+import Testimonial from './components/Testimonial'
+import Work from './components/Work'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+    <div>
+      <Home />
+      <About />
+      <Work />
+      <Testimonial />
+      <Contact />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
